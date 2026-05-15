b054c0f97d507287bfd7a03cdcdbbacc19b0a1d6
d4niel <dhaddad3@students.avc.edu>
2026-05-14 22:06:35 -0700

    commit 1

--- main.hpp
+++ main.hpp
@@ -1,8 +1,10 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
+
 // Construct an array-of-records from a multi-column data file (babyname.txt)
 // and search for matching names by state and starting character.
+
 
 #include <iostream>
 #include <fstream>
@@ -10,22 +12,52 @@
 #include <string>
 using namespace std;
 
+
 const int N = 5000;
+
 
 int makeNameRecord(string[], string[], int[], string[], int[]);
 void printoutallrecords(int, string[], string[], int[], string[], int[]);
 int findNames(int, string[], string[], int[], string[], int[], char, string);
 void printoutcontents(string, string, int, string, int);
 
+
 int makeNameRecord(string state[], string gender[], int year[], string name[], int count[])
 {
-    // TODO: open "babyname.txt", read each line into the parallel arrays,
+    ifstream infile("babyname.txt");
+    if (!infile)
+    {
+        cerr << "Error opening babyname.txt" << endl;
+        return 0;
+    }
+
+    int cnt = 0;
+    while (cnt < N &&
+           (infile >> state[cnt] >> gender[cnt] >> year[cnt] >> name[cnt] >> count[cnt]))
+    {
+        cnt++;
+    }
+
+    return cnt;
 }
+
 
 int findNames(int cnt, string state[], string gender[], int year[], string name[], int count[], char starting, string stname)
 {
-    // TODO: print each record where the state matches `stname` and the name
+    int matches = 0;
+
+    for (int i = 0; i < cnt; ++i)
+    {
+        if (state[i] == stname && name[i].rfind(starting, 0) == 0)
+        {
+            printoutcontents(state[i], gender[i], year[i], name[i], count[i]);
+            matches++;
+        }
+    }
+
+    return matches;
 }
+
 
 void printoutallrecords(int cnt, string state[], string gender[], int year[], string name[], int count[])
 {
@@ -33,6 +65,7 @@ void printoutallrecords(int cnt, string state[], string gender[], int year[], st
         printoutcontents(state[i], gender[i], year[i], name[i], count[i]);
     cout << "total lines: " << cnt << endl;
 }
+
 
 void printoutcontents(string statename, string sex, int year, string name, int count)
 {
@@ -42,5 +75,6 @@ void printoutcontents(string statename, string sex, int year, string name, int c
     cout << setw(10) << name << "\t\t";
     cout << setw(5) << count << endl;
 }
+
 
 #endif
